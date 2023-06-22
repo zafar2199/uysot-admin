6a89bdad2776d7d11c1f55c1a48a83d1dc361e61
zafar2199 <zrozmatov0@gmail.com>
2023-06-23 02:10:02 +0500

    feat: install sass and style folder add scss file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { LoginPage } from "@pages/index";
 
+import "@styles/main.scss";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <>
